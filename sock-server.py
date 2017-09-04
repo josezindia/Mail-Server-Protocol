3fad08c5f087bb056d2f1513466748a8a96c4ab4
Jose Zindia <josezindia36@gmail.com>
2017-09-04 11:07:06 -0400

    Update sock-server.py

--- sock-server.py
+++ sock-server.py
@@ -1,7 +1,7 @@
 #------------------------------------------------------------------------------
 # CSC 412-Networking
 # Expanding A Protocol 
-# Authors: Sher Sanginov,Jose Zindia, Dary Sullivan, Andrew Mambondiumwe & Johanathan Chauwa
+# Authors: Jose Zindia
 #------------------------------------------------------------------------------
 
 import threading
